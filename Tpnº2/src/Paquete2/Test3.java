68ebea5f188fd4f867e08abe1c7df25c86ffd45f
sebacasca <sebacasca@outlook.com>
2014-09-13 15:55:39 -0300

    paquete 2 listo

--- Tpnº2/src/Paquete2/Test3.java
+++ Tpnº2/src/Paquete2/Test3.java
@@ -4,14 +4,26 @@ public class Test3 {
 
 	public static void main(String[] args) {
 		
-		int n=25;
-		for(int i=1; i<=n; i++){
-			
+		// Numeros primos de 1 hasta n con WHILE
+		
+		int n=125;
+		int i=1;
+		while(n>=i){
 			if(Primo.esPrimo(i)){
-				
 				System.out.print("\n"+i);}
-				}
-		}
+		i++;}
+		
+		
+		//Numeros primos de 1 hasta n con FOR
+		
+		/*int n=125;
+		for(int i=1; i<=n; i++){
+			if(Primo.esPrimo(i)){
+				System.out.print("\n"+i);}
+				}*/
 	}
+				
+}
+	
 
 
--- Tpnº2/src/Paquete2/Test4.java
+++ Tpnº2/src/Paquete2/Test4.java
@@ -1,26 +1,37 @@
 package Paquete2;
-
+import Paquete2.Primo;
 public abstract class Test4 {
-
+	
 	public static void main(String[] args) {
-		int n=100;
-		int aux=1, aux2=1;
-		int c1=1;
-		int n1=1;
-		int n2=1;
-		while(! (c1<n)){
-			n1++;
-			n2++;
-			if(Primo.esPrimoMellizo(n1, n2)==true){
-				aux=n1;
-				aux2=n2;}
-			c1++;
-			System.out.println(aux+ aux2);
+	
+	//Numeros primos mellizos de 1 hasta n con FOR
+	
+	int n=125;
+	for(int i=2; i<=n; i++){
+			for(int j=1; j<=n; j+=2){
+				if(Primo.esPrimoMellizo(i,j)){
+			
+			System.out.print("\n"+i);
+			System.out.println("\n"+j);}
+			}
 		}
+	
+	
+	//Numeros primos mellizos de 1 hasta n con WHILE
+	
+	/*int x=125;
+	int i=2,j=1;
+	while((x>=i)||(x>=j)){
+		if(Primo.esPrimoMellizo(i, j)){
+			System.out.print("\n"+i);
+			System.out.println("\n"+j);}
+			i++;
+			j+=2;}*/
 		
-		}
-		
-
 	}
+}
+
+
+
 
 
